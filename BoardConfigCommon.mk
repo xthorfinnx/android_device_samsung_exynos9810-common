402400b1e3dcdb28826177f7761f2e5c03ac26f0
Thorfinn <xxthorfinnxx@gmail.com>
2026-09-25 15:45:00 +0500

    exynos9810-common: build /system and /vendor as EROFS
    Switch the system and vendor images from ext4 to EROFS with lz4hc,9 compression and a 16 KiB physical cluster size.
    
    - Set BOARD_{SYSTEM,VENDOR}IMAGE_FILE_SYSTEM_TYPE to erofs.
    - Add erofs fstab entries for SYSTEM and VENDOR, keeping the ext4
      entries as fallback so first-stage mount works with either image.
    
    Signed-off-by: Thorfinn <xxthorfinnxx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,12 +49,17 @@ BOARD_MKBOOTIMG_ARGS := --header_version 0 \
 $(call soong_config_set_bool,samsungCameraVars,usage_64bit,true)
 $(call soong_config_set_bool,samsungCameraVars,needs_sec_reserved_field,true)
 
+# Compression
+BOARD_EROFS_COMPRESSOR := lz4hc,9
+BOARD_EROFS_PCLUSTER_SIZE := 16384
+
 # Display
 BOARD_MINIMUM_DISPLAY_BRIGHTNESS := 1
 
 # Filesystem
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_ODM := vendor/odm
